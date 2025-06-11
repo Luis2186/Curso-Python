4c71e7238185df4eb8a3e89cc4c109e70a97dd8e
Luis Lopez <l.lopezperdomo.e@gmail.com>
2025-06-11 12:37:52 -0300

    Primer commit

---
+++ Primer Proyecto/Primer_Proyecto.py
@@ -0,0 +1,6 @@
+print("!Creemos tu nuevo nombre creativo!")
+
+preguntaUno= input("¿Cuál es tu color favorito? ")
+preguntaDos= input("¿Cuál es el nombre de tu mascota? ")
+
+print(f"Tu nuevo nombre creativo es: '{preguntaUno} {preguntaDos}' ")
