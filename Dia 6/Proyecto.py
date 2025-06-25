492a90d9c534f555abaf6bc838a2f5ceb6596553
Luis Lopez <109772269+Luis2186@users.noreply.github.com>
2025-06-25 10:09:08 -0300

    Add files via upload
    Dia 6 Finalizado Proyecto

--- Dia 6/Proyecto.py
+++ Dia 6/Proyecto.py
@@ -1,6 +1,8 @@
 from pathlib import Path, PureWindowsPath
 
-ruta = Path("B:\Cursos-Programacion\Curso Python\Dia 6\Recetas")
+#ruta = Path("B:\Cursos-Programacion\Curso Python\Dia 6\Recetas")
+ruta = Path("D:\Cursos Programacion\Curso-Python\Dia 6\Recetas")
+abecedario='abcedefghijklmnopqrstuvwxyz'
 
 print("Bienvenido al recetario")
 
@@ -49,56 +51,122 @@ def iniciar_programa_recetas():
             print("Opción inválida. Por favor, selecciona una opción del 1 al 6.")
 
 
-def obtener_sub_directorio(ruta):
-    sub_directorios = [item.name for item in ruta.iterdir() if item.is_dir()]
-    return list(enumerate(sub_directorios)) 
+# def obtener_sub_directorio(ruta):
+#     sub_directorios = [item.name for item in ruta.iterdir() if item.is_dir()]
+#     return list(enumerate(sub_directorios)) 
 
+def obtener_directorio(ruta):
+    sub_directorios_rutas = [str(item.resolve()) for item in ruta.iterdir() if item.is_dir()]
+    sub_directorios_nombres = [item.name for item in ruta.iterdir() if item.is_dir()]
+
+    return list(enumerate(sub_directorios_nombres)), sub_directorios_rutas
 
 def obtener_menu_sub_directorio(sub_directorios):
-    return "\n".join([f"{categoriaNro}: {categoria}" for categoriaNro, categoria in sub_directorios])
+
+    opciones ="\n".join([f"{categoriaNro}: {categoria}" for categoriaNro, categoria in sub_directorios])
+    return opciones
+
+def obtener_posicion_opcion_elegida(directorio):
+    opciones = obtener_menu_sub_directorio(directorio)
+    cantidad_opciones = len(directorio)
+    
+    while True:
+        try:
+            opcion_elegida = int(input(f"Por favor elija una opción:\n{opciones}\n> "))
+            if 0 <= opcion_elegida < cantidad_opciones:
+                return opcion_elegida
+            else:
+                print("Opción fuera de rango. Intente nuevamente.")
+        except ValueError:
+            print("Entrada inválida. Por favor, ingrese un número entero.")
+
+def salir_de_menu(letra):
+    while letra not in abecedario:
+        letra = input("Para salir precione cualquier letra")    
+    
+
+def obtener_ruta_directorio_elegido(sub_directorios_nombres,sub_directorios_rutas):  
+    opcion_posicion =obtener_posicion_opcion_elegida(sub_directorios_nombres)
+    opcion_elegida= sub_directorios_rutas[opcion_posicion]
+    return opcion_elegida
 
 
-def obtener_ruta_sub_directorio_elegido(sub_directorios):
-    opciones= obtener_menu_sub_directorio(sub_directorios)
-    categoria =int(input(f"Por favor elija una categoría:\n{opciones}\n> "))
-    categoria_elegida= sub_directorios[categoria][1]
-    ruta_directorio_elegido =  [item for item in ruta.iterdir() if item.is_dir() and item.name == categoria_elegida ]
-    return str(ruta_directorio_elegido[0].resolve())
+def leer_receta_elegido(ruta_archivo):
+    archivo = open(ruta_archivo)
+    print(archivo.read())
 
-# def obtener_ruta_archivo_elegido(archivos):
-#     opciones= obtener_menu_sub_directorio(archivos)
-#     categoria =int(input(f"Por favor elija una categoría:\n{opciones}\n> "))
-#     categoria_elegida= sub_directorios[categoria][1]
-#     ruta_directorio_elegido =  [item for item in ruta.iterdir() if item.is_dir() and item.name == categoria_elegida ]
-#     return str(ruta_directorio_elegido[0].resolve())
+def crear_receta_archivo(ruta_categoria_elegida,nombre_receta,contenido_receta):
+    ruta_receta_nueva = Path(ruta_categoria_elegida,nombre_receta)
+    archivo = open(ruta_receta_nueva ,'w')
+    archivo.write(contenido_receta)
+    archivo.close()
+
+def eliminar_receta_elegida(ruta_archivo):
+    ruta_archivo = Path(ruta_archivo)
+    ruta_archivo.unlink()
+
+def eliminar_categoria_elegida(ruta_categoria):
+    ruta_categoria = Path(ruta_categoria)
+    ruta_categoria.rmdir()
+
+def crear_categoria_nueva(ruta,nombre_categoria):
+    ruta_categoria_nueva = Path(ruta,nombre_categoria)
+    ruta_categoria_nueva.mkdir(parents=True, exist_ok=True)
+
 
 def obtener_archivos_directorio(ruta):
     p = Path(ruta)  # ← Convertir string a Path
+    archivos_directorio = [str(item.resolve()) for item in p.iterdir() if item.is_file() and item.suffix == ".txt"]
+
     archivos = [item.stem for item in p.iterdir() if item.is_file() and item.suffix == ".txt"]
-    return list(enumerate(archivos))
+    return list(enumerate(archivos)),archivos_directorio
 
 def leer_receta():
-    letra =''
-    sub_directorio= obtener_sub_directorio(ruta)
-    ruta_categoria_elegida = obtener_ruta_sub_directorio_elegido(sub_directorio)
-    archivos= obtener_archivos_directorio(ruta_categoria_elegida)
-    opciones_archivo = obtener_menu_sub_directorio(archivos)
-    print(opciones_archivo)
-    while letra != "a":
-        letra = input("Para salir precione la letra a")    
-    
+    letra ='1'
+    sub_directorio_nombres, sub_directorio_rutas= obtener_directorio(ruta)
+    ruta_categoria_elegida = obtener_ruta_directorio_elegido(sub_directorio_nombres,sub_directorio_rutas)
+
+    archivos_nombres,archivos_directorio= obtener_archivos_directorio(ruta_categoria_elegida)
+    ruta_archivo = obtener_ruta_directorio_elegido( archivos_nombres,archivos_directorio)
+    leer_receta_elegido(ruta_archivo)
+
+    salir_de_menu(letra)
     return
 
 def crear_receta():
+    
+    letra ='1'
+    sub_directorio_nombres, sub_directorio_rutas= obtener_directorio(ruta)
+    ruta_directorio_elegido = obtener_ruta_directorio_elegido(sub_directorio_nombres,sub_directorio_rutas)
+    nombre_receta=input("Ingrese el nombre de la receta: ") + ".txt"
+    contenido_receta=input("Ingrese la receta: ")
+    crear_receta_archivo(ruta_directorio_elegido,nombre_receta,contenido_receta)
+    salir_de_menu(letra)
     return
 
 def crear_categoria():
+    letra ='1'
+    categoria_nueva= input("Ingrese el nombre de la nueva categoria: ")
+    crear_categoria_nueva(ruta,categoria_nueva)
     return
 
 def eliminar_receta():
+    letra ='1'
+    sub_directorio_nombres, sub_directorio_rutas= obtener_directorio(ruta)
+    ruta_categoria_elegida = obtener_ruta_directorio_elegido(sub_directorio_nombres,sub_directorio_rutas)
+
+    archivos_nombres,archivos_directorio= obtener_archivos_directorio(ruta_categoria_elegida)
+    ruta_archivo = obtener_ruta_directorio_elegido( archivos_nombres,archivos_directorio)
+    eliminar_receta_elegida(ruta_archivo)
+    salir_de_menu(letra)
     return
 
 def eliminar_categoria():
+    letra ='1'
+    sub_directorio_nombres, sub_directorio_rutas= obtener_directorio(ruta)
+    ruta_categoria_elegida = obtener_ruta_directorio_elegido(sub_directorio_nombres,sub_directorio_rutas)
+    eliminar_categoria_elegida(ruta_categoria_elegida)
+    salir_de_menu(letra)
     return
 
 
